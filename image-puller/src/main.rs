f039acaf4bc41bbb7d6badc7911387b1bc1a22b9
AidoP <aidop@me.com>
2020-10-18 18:46:57 +0800

    Image puller correctly crops and categorises

--- image-puller/src/main.rs
+++ image-puller/src/main.rs
@@ -1,24 +1,31 @@
-//use std::{fs::File, io::{self,Read}, path::Path};
+use std::{fs::File, io::{self,BufRead,BufReader}};
 use std::path::PathBuf;
-use std::collections::{HashMap, VecDeque};
-use tokio::{self,fs::File,io::{self,AsyncWriteExt,AsyncRead,AsyncBufRead,AsyncBufReadExt,BufReader}};
+use std::collections::HashMap;
+//use tokio::{self,fs::File,io::{self,AsyncWriteExt,AsyncRead,AsyncBufRead,AsyncBufReadExt,BufReader}};
 use reqwest;
+use image;
 
-const IMAGE_GET_REQUESTS: usize = 3;
+const REQUIRED_CONFIDENCE: f32 = 0.5;
+
+struct BoundingBox<'a> {
+    label: &'a str,
+    confidence: f32,
+    points: (f32, f32, f32, f32)
+}
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let labels_file = BufReader::new(File::open("sources/labels.csv").await?);
-    let categories_file = BufReader::new(File::open("sources/categories.csv").await?);
-    let boxes_file = BufReader::new(File::open("sources/bounding-boxes.csv").await?);
-    let images_file = BufReader::new(File::open("sources/images.csv").await?);
+    let labels_file = BufReader::new(File::open("sources/labels.csv").unwrap());
+    let categories_file = BufReader::new(File::open("sources/categories.csv").unwrap());
+    let boxes_file = BufReader::new(File::open("sources/bounding-boxes.csv").unwrap());
+    let images_file = BufReader::new(File::open("sources/images.csv").unwrap());
 
     
     // Find man and woman labels
     let (woman_label, man_label) = {
-        let mut lines = labels_file.lines();
         let (mut woman_label, mut man_label) = (None, None);
-        while let Some(line) = lines.next_line().await? {
+        for line in labels_file.lines() {
+            let line = line?;
             let mut parts = line.split(",");
             let category = parts.next().map(|s| s.to_owned());
             let label = parts.next().expect("Invalid labels file");
@@ -30,6 +37,38 @@ async fn main() -> io::Result<()> {
         }
         (woman_label.expect("Unable to find woman label"), man_label.expect("Unable to find man label"))
     };
+    
+    // Get image bounding boxes
+    let bounding_boxes = {
+        let mut boxes = HashMap::<String, Vec::<_>>::new();
+        for line in boxes_file.lines() {
+            let line = line?;
+            let mut parts = line.split(",");
+            let id = parts.next().map(|s| s.to_owned()).expect("Invalid image id in boxes file");
+            let mut parts = parts.skip(1);
+            let label = parts.next().expect("Invalid boxes file");
+            let new_box = BoundingBox {
+                label: match label {
+                    label if label == woman_label => &woman_label,
+                    label if label == man_label => &man_label,
+                    _ => continue
+                },
+                confidence: parts.next().expect("Invalid boxes file").parse().expect("Confidence is not a float"),
+                points: (
+                    parts.next().expect("Invalid boxes file").parse().expect("XMin is not a float"),
+                    parts.next().expect("Invalid boxes file").parse().expect("XMax is not a float"),
+                    parts.next().expect("Invalid boxes file").parse().expect("YMin is not a float"),
+                    parts.next().expect("Invalid boxes file").parse().expect("YMax is not a float")
+                )
+            };
+            if let Some(boxes) = boxes.get_mut(&id) {
+                boxes.push(new_box)
+            } else {
+                boxes.insert(id, vec![new_box]);
+            }
+        }
+        boxes
+    };
 
     // Get images links
     let images_future = get_images(images_file);
@@ -39,16 +78,16 @@ async fn main() -> io::Result<()> {
     let images = images_future.await?;
     let (women, men) = ids_future.await?;
     
-    save_all(women, &images).await.unwrap_or_else(|e| eprintln!("Error getting image: {:?}", e));
-    save_all(men, &images).await.unwrap_or_else(|e| eprintln!("Error getting image: {:?}", e));
+    save_all(&woman_label, &man_label, women.into_iter().chain(men.into_iter()), &images, &bounding_boxes).await.unwrap_or_else(|e| eprintln!("Error getting image: {:?}", e));
+    //save_all(men, &images, &bounding_boxes).await.unwrap_or_else(|e| eprintln!("Error getting image: {:?}", e));
 
     Ok(())
 }
 
 async fn get_images(images_file: BufReader<File>) -> io::Result<HashMap<String, String>> {
     let mut images = HashMap::new();
-    let mut lines = images_file.lines();
-    while let Some(line) = lines.next_line().await? {
+    for line in images_file.lines() {
+        let line = line?;
         let mut parts = line.split(",");
         if let Some(_) = images.insert(
             parts.next().unwrap().to_owned(),
@@ -61,8 +100,8 @@ async fn get_images(images_file: BufReader<File>) -> io::Result<HashMap<String, 
 }
 async fn get_ids(categories_file: BufReader<File>, woman_label: &str, man_label: &str) -> io::Result<(Vec<String>, Vec<String>)> {
     let mut ids = (vec![], vec![]);
-    let mut lines = categories_file.lines();
-    while let Some(line) = lines.next_line().await? {
+    for line in categories_file.lines() {
+        let line = line?;
         let mut parts = line.split(",");
         let id = parts.next().unwrap().to_owned();
         match parts.skip(1).next().unwrap() {
@@ -74,19 +113,38 @@ async fn get_ids(categories_file: BufReader<File>, woman_label: &str, man_label:
     Ok(ids)
 }
 
-async fn save_all(ids: Vec<String>, images: &HashMap<String, String>) -> reqwest::Result<()> {
-    let mut get_futures = VecDeque::new();
-    for image in ids {
-        get_futures.push_back((image.clone(), reqwest::get(&images[&image])));
-        if get_futures.len() > IMAGE_GET_REQUESTS {
-            let (mut id, image) = get_futures.pop_front().unwrap();
+async fn save_all<'a>(woman_label: &str, man_label: &str, ids: impl Iterator<Item=String>, images: &HashMap<String, String>, bounding_boxes: &HashMap<String, Vec<BoundingBox<'a>>>) -> reqwest::Result<()> {
+    for image_id in ids {
+        if let Some(boxes) = bounding_boxes.get(&image_id) {
+            let image_request = reqwest::get(&images[&image_id]);
             let mut path = PathBuf::new();
             path.push("images");
-            id.push_str(".png");
-            path.push(id);
-            println!("Saving image {:?}", path);
-            let mut file = File::create(path).await.expect("Unable to create image file");
-            file.write_all(&*image.await?.bytes().await?).await.expect("Unable to write to image file");
+
+            use image::GenericImageView;
+            println!("Getting image...");
+            match image::load_from_memory(&*image_request.await?.bytes().await?) {
+                Ok(image) => {
+                    let (width, height) = image.dimensions();
+                    let (width, height) = (width as f32, height as f32);
+                    for (index, bounding_box) in boxes.iter().enumerate() {
+                        // Skip if a bad photo
+                        if bounding_box.confidence < REQUIRED_CONFIDENCE { continue }
+
+                        let mut path = path.clone();
+                        path.push(match bounding_box.label {
+                            label if label == woman_label => "women",
+                            label if label == man_label => "men",
+                            _ => panic!("This label should be pruned")
+                        });
+                        let (x, y) = (bounding_box.points.0 * width, bounding_box.points.2 * height);
+                        let (width, height) = (bounding_box.points.1 * width - x, bounding_box.points.1 * width - x);
+                        let image = image.crop_imm(x as _, y as _, width as _, height as _);
+                        path.push(&format!("{}-{}.png", image_id, index));
+                        image.save(path).unwrap_or_else(|e| eprintln!("Unable to save image: {}", e));
+                    }
+                }
+                Err(error) => eprintln!("Unable to load image: {:?}", error)
+            }
         }
     }
 
